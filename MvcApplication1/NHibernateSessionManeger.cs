ab545351bd3025146c92c969be0a24074daf1b6e
Daniil Sozontov <continue@teknik.io>
2019-02-08 10:09:06 +0300

    first commit

---
+++ MvcApplication1/NHibernateSessionManeger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Cfg;
+using NHibernate;
+namespace MvcApplication1
+{
+    public class NHibernateSessionManeger
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var bookConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Mappings\Users.hbm.xml");
+            configuration.AddFile(bookConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
